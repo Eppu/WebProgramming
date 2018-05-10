05bdc00fa77c30630c0eb1397f3a2307328f5fb5
Eetu Eskelinen <edwardtehgreat@gmail.com>
2018-05-10 15:49:21 +0300

    Added school line button functionality.

--- Classwork/excercises_completed/BusTrackerApp/js/busTracker1_newest.js
+++ Classwork/excercises_completed/BusTrackerApp/js/busTracker1_newest.js
@@ -12,13 +12,15 @@ const INIT_ZOOM = 13;
 const UPDATE_FREQ = 1000;
 
 //data source
-let busDataSource = "http://lissu-api.herokuapp.com/";
+//let busDataSource = "http://lissu-api.herokuapp.com/";
+let busDataSource = "./testdata.json";
 
 //html element references
 let clearArrayBtn = document.getElementById("cleararraybutton");
 let clearBtn = document.getElementById("clearbutton");
 let modal = document.getElementById('myModal');
-let modalBtn =  document.getElementById("myBtn");
+let modalBtn = document.getElementById("myBtn");
+let geolocBtn = document.getElementById("geolocationButton");
 let modalClBtn = document.getElementById("modalCloseBtn");
 let mapDivElem = document.getElementById("mapholder");
 let mediapolisBtn = document.getElementById("mediapolisButton");
@@ -30,12 +32,17 @@ let myMap;
 let geocoder = new google.maps.Geocoder();
 let directionsService = new google.maps.DirectionsService();
 let directionsDisplay = new google.maps.DirectionsRenderer();
+let infoWindow = new google.maps.InfoWindow;
 
 //arrays to store marker information as well as the chosen bus lines
 let markers = new Array();
 //show these lines by default
 let chosenBusLines = ["8", "17", "5"];
 //let deployedBusLines = new Array();
+
+let mediapolisLines = ["8", "17"];
+let tamkLines = ["1", "5", "8", "28A", "28B", "28C", "28Y", "29", "80", "90"];
+let universityLines = ["2", "6", "9A", "9B", "15", "28B", "40"];
 
 let gotInitialValues = false;
 
@@ -51,47 +58,52 @@ class BusMarker {
 //Event listeners
 clearBtn.addEventListener("click", clearMarkers);
 
+geolocBtn.addEventListener("click", locateUser);
+
 //clear the chosen lines, clear all the markers and remove the selected classes from buttons
-clearArrayBtn.addEventListener("click", function(){
-  document.querySelectorAll(".line-btn").forEach(function(elem) {
-    elem.classList.remove("selected");
-  });
+clearArrayBtn.addEventListener("click", function() {
+  clearButtons();
   chosenBusLines.length = 0;
   clearMarkers();
 });
 //on a click of the modalBtn, show the modal menu
 modalBtn.onclick = function() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 //on click of the modalClBtn, hide the modal menu
 modalClBtn.onclick = function() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
-mediapolisBtn.addEventListener("click", function() {
- //Add lines going to Mediapolis into the array if they're not on there already. (8 and 17). Do the same for the other schools in other listeners.
- //Also display a marker where the school is?
-        //chosenBusLines = chosenBusLines.filter(e => e !== "17");
-        console.log(chosenBusLines);
+mediapolisBtn.addEventListener("click", function(){
+  showSchoolLines(mediapolisLines);
+});
+
+tamkBtn.addEventListener("click", function(){
+  showSchoolLines(tamkLines);
+});
+
+universityBtn.addEventListener("click", function(){
+  showSchoolLines(universityLines);
 });
 
 //Add event listeners to all elements with the "line-btn" class
 function addListeners() {
-    document.querySelectorAll(".line-btn").forEach(function(elem) {
-        elem.addEventListener("click", function() {
-          //if the array already includes the value of the current elem, remove it
-          if(chosenBusLines.includes(elem.value)){
-            removeFromSelection(elem);
-          //otherwise push it to the array
-          } else {
-            addToSelection(elem);
-          }
-        });
+  document.querySelectorAll(".line-btn").forEach(function(elem) {
+    elem.addEventListener("click", function() {
+      //if the array already includes the value of the current elem, remove it
+      if (chosenBusLines.includes(elem.value)) {
+        removeFromSelection(elem);
+        //otherwise push it to the array
+      } else {
+        addToSelection(elem);
+      }
     });
+  });
 }
 
 //add the passed in element's value to the chosen bus lines. After that, reload to make sure no duplicate markers stay on the map.
-function addToSelection(elem){
+function addToSelection(elem) {
   clearMarkers();
   chosenBusLines.push(elem.value);
   console.log(chosenBusLines);
@@ -101,7 +113,7 @@ function addToSelection(elem){
 }
 
 //filter out the passed in element's value from the chosen bus lines. After that, reload the buses to make sure no duplicate markers stay on the map.
-function removeFromSelection(elem){
+function removeFromSelection(elem) {
   chosenBusLines = chosenBusLines.filter(e => e !== elem.value);
   clearMarkers();
   console.log(chosenBusLines);
@@ -110,14 +122,13 @@ function removeFromSelection(elem){
   getJSONData();
 }
 
-function checkIfSelected(thisElement){
-  if(chosenBusLines.includes(thisElement.value)){
+function checkIfSelected(thisElement) {
+  if (chosenBusLines.includes(thisElement.value)) {
     thisElement.classList.add("selected");
   } else {
     thisElement.classList.remove("selected");
   }
 }
-
 
 
 
@@ -127,12 +138,12 @@ showMap();
 checkInitialSelections();
 
 
-function checkInitialSelections(){
-window.onload = function() {
-      document.querySelectorAll(".line-btn").forEach(function(elem) {
-        checkIfSelected(elem);
-      });
-};
+function checkInitialSelections() {
+  window.onload = function() {
+    document.querySelectorAll(".line-btn").forEach(function(elem) {
+      checkIfSelected(elem);
+    });
+  };
 }
 
 //fetch the data from the API
@@ -147,14 +158,20 @@ function getJSONData() {
         }
         response.json().then(function(data) {
           //if the data hasn't been fetched before, run the storeBusData function.
-          if (!gotInitialValues) {
-            //display_status("number of items found " + data.vehicles.length);
-            data.vehicles.forEach(storeBusData);
-            gotInitialValues = true;
-            //if the data has already been stored once, run the updateBusData function instead.
+          if (data.vehicles !== undefined) {
+            if (!gotInitialValues) {
+              //display_status("number of items found " + data.vehicles.length);
+              data.vehicles.forEach(storeBusData);
+              gotInitialValues = true;
+              //if the data has already been stored once, run the updateBusData function instead.
+            } else {
+              //display_status("number of items found " + data.vehicles.length);
+              data.vehicles.forEach(updateBusData);
+            }
           } else {
-            //display_status("number of items found " + data.vehicles.length);
-            data.vehicles.forEach(updateBusData);
+            console.log("Error. Bus data not found. Try again in a few minutes.");
+            return;
+
           }
         });
       }
@@ -173,6 +190,7 @@ function showMap() {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     fullscreenControl: false,
+    streetViewControl: false,
     navigationControlOptions: {
       style: google.maps.NavigationControlStyle.SMALL
     },
@@ -217,6 +235,60 @@ function busAlreadyHasMarker(busItem) {
   }
 }
 
+function locateUser() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      //if location is found, display a message saying so
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(myMap);
+      myMap.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, myMap.getCenter());
+    });
+  } else {
+    // browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, myMap.getCenter());
+  }
+}
+// Send the user an error message if the geolocating failed, or if the browser doesn't support geolocation.
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed. Try turning on your location data.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(myMap);
+}
+
+function clearButtons(){
+document.querySelectorAll(".line-btn").forEach(function(elem) {
+  elem.classList.remove("selected");
+});
+}
+
+function showSchoolLines(lineArray){
+  //first remove all lines from the arrays
+  chosenBusLines.length = 0;
+  clearButtons();
+  clearMarkers();
+  for(i = 0; i < lineArray.length; i++) {
+    chosenBusLines.push(lineArray[i]);
+  }
+  //lines to the university: 2 6 9A 9B 15 28B 40
+  //chosenBusLines.push("2", "6", "9A", "9B", "15", "28B", "40");
+  //set the activated lines' buttons to the selected state
+  document.querySelectorAll(".line-btn").forEach(function(elem) {
+    checkIfSelected(elem);
+  });
+  gotInitialValues = false;
+  console.log(chosenBusLines);
+  getJSONData();
+}
+
 //set the interval of calling getJSONData to the UPDATE_FREQ constant (1000ms)
 setInterval(getJSONData, UPDATE_FREQ);
 
@@ -231,10 +303,10 @@ setInterval(getJSONData, UPDATE_FREQ);
 // Removes the markers from the map, but keeps them in the array.
 function clearMarkers() {
   console.log("Got click");
-  for (i = 0; i < markers.length; i++){
-  markers[i].mapMarker.setMap(null);
+  for (i = 0; i < markers.length; i++) {
+    markers[i].mapMarker.setMap(null);
   }
-    markers.length = 0;
+  markers.length = 0;
 }
 
 /*-------------------------------------------------------
